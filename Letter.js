7167cde5babd5a3c00116f0c897fbbb15b83c5bf
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-02-02 02:21:39 -0500

    first commit

---
+++ Letter.js
@@ -0,0 +1,4 @@
+// Coder: Ivonne Komis
+// constructor file Letter.js should contain all methods to check 
+// should control whether or not a letter appears as 
+// a "_" or as itself on-screen.
---
+++ Word.js
@@ -0,0 +1,4 @@
+// Coder: Ivonne Komis
+// constructor file Word.js should contain all methods to check 
+// LETTERS GUESSED versus the RANDOM WORD Selected
+
